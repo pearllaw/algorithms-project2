6d7a1dc767dd34dd9900404d360dd45b703413d0
Pearl Law <pearllynnelaw@gmail.com>
2020-09-28 14:17:30 -0700

    Implement timer for efficiency testing

--- maxdefense_main.cc
+++ maxdefense_main.cc
@@ -1,10 +1,21 @@
-
-
 #include "maxdefense.hh"
+#include "timer.hh"
 
+using namespace std;
 
 int main()
 {
+	int n = 8000;	// adjust n to return different filtered_armors size
+	auto armors = load_armor_database("armor.csv");
+	auto filtered_armors = filter_armor_vector(*armors, 1, 100, n);
+	
+	// Testing time for each algorithm (comment out section not being tested)
+	Timer timer;
+	greedy_max_defense(*filtered_armors, 5000);
+	// exhaustive_max_defense(*filtered_armors, 5000);
+	double elapsed = timer.elapsed();
+	cout << "Elapsed time in seconds: " << elapsed << endl;
+	
 	return 0;
 }
 
