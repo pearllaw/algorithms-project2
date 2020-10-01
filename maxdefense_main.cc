0e43f430fe1aeaf81aee776a8d2ca87c16fcbde5
Pearl Law <pearllynnelaw@gmail.com>
2020-10-01 16:12:02 -0700

    edit testing code

--- maxdefense_main.cc
+++ maxdefense_main.cc
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-	int n = 8000;	// adjust n to return different filtered_armors size
+	int n = 100;	// adjust n to return different filtered_armors size
 	auto armors = load_armor_database("armor.csv");
 	auto filtered_armors = filter_armor_vector(*armors, 1, 100, n);
 	
-	// Testing time for each algorithm (comment out section not being tested)
+	// Testing time for each algorithm given total cost = 5000 
+	// Commented out section not being tested
 	Timer timer;
 	greedy_max_defense(*filtered_armors, 5000);
 	// exhaustive_max_defense(*filtered_armors, 5000);
